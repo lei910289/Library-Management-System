d3c747907a05f16675d427a017a9e5b7b96535ef
lei910289 <lei910829@gmail.com>
2016-11-07 18:00:43 -0600

    Add files via upload

---
+++ schema creation file/library_LIBRARY_BRANCH.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
+--
+-- Host: localhost    Database: library
+-- ------------------------------------------------------
+-- Server version	5.5.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `LIBRARY_BRANCH`
+--
+
+DROP TABLE IF EXISTS `LIBRARY_BRANCH`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `LIBRARY_BRANCH` (
+  `Branch_id` int(11) NOT NULL,
+  `Branch_name` varchar(80) DEFAULT NULL,
+  `Address` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`Branch_id`),
+  UNIQUE KEY `Branch_name_UNIQUE` (`Branch_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `LIBRARY_BRANCH`
+--
+
+LOCK TABLES `LIBRARY_BRANCH` WRITE;
+/*!40000 ALTER TABLE `LIBRARY_BRANCH` DISABLE KEYS */;
+INSERT INTO `LIBRARY_BRANCH` VALUES (1,'Oak Lawn','4100 Cedar Springs Road, 75219'),(2,'Lakewood','6121 Worth Street, 75214'),(3,'Grauwyler Park','2146 Gilford Street, 75235'),(4,'Highland Hills','3624 Simpson Stuart Road, 75241'),(5,'Audelia Road','10045 Audelia Road, 75238');
+/*!40000 ALTER TABLE `LIBRARY_BRANCH` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-10-25 14:55:55
